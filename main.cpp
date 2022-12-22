ddb52d63d13d94e0d573d9d4c7cb02425439173a
Edgeeee11 <arsenykurenkov@gmail.com>
2022-12-22 19:39:08 +0300

    Homework

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+
+main() 
+{
+	char str[100];
+	
+	printf("Please enter a word\n");
+	
+	gets(str);
+	
+	for(int j=0; j<strlen(str); j++)
+	{
+		str[j]=str[j+1];
+	}
+	
+	printf("Result\n");
+	puts(str);
+	
+	
+	
+	char strr[100];
+
+    for(int j=strlen(str);j>0;j--)
+    {
+        strr[strlen(str)-j] = str[j-1];       
+    }
+    printf("Result\n");
+    puts(strr);
+}
+	
+
